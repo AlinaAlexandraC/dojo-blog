b27307b44fdfa2d73a30096a7744746cda30309a
AlinaAlexandraC <127543454+AlinaAlexandraC@users.noreply.github.com>
2024-01-20 15:13:42 +0200

    push again

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 import useFetch from './useFetch';
+import db from './data/db.json';
 
 const Home = () => {
-    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
-
+    // const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+    console.log(db);
     return (
         <div className="home">
-            { error && <div> { error} </div>}
-            { isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={ blogs } title="All blogs" />}
+            {/* { error && <div> { error} </div>}
+            { isPending && <div>Loading...</div> } */}
+            {db.blogs && <BlogList blogs={ db.blogs } title="All blogs" />}
         </div>
     );
 }
